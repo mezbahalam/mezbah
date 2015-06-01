c2f3413f7ac99bd3e0187f68e30f96bd3a2fa2eb
mezbahalam <mezbahalam26@gmail.com>
2015-06-01 15:37:07 +0600

    provide outh

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,6 +7,3 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV["REDISTOGO_URL"] || "redis://localhost:6379/" }
 end
-if Rails.env.test?
-  Sidekiq.logger.level = Logger::WARN
-end
